f9460f06afbbc1c75d321630f494acc60e04063f
Konstantin1spb <kevlen02@mail.ru>
2022-04-11 20:05:38 +0300

    estetics 1.0
    Верстка главной страницы

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('.first-slider').slick({
+  	autoplay: true,
+    autoplaySpeed: 4000
+  });
+
+  $('.new__slider').slick({
+  	autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+
+  $('.week__slider').slick({
+  	autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+});
